3f782f8e50124ab7895a6e6598b083a3f678079a
Nicholas Nuing <nicholas.nuing@gmail.com>
2016-04-25 07:45:59 +0800

    Added REST API calls to the viewDidLoad function.

--- funWithSegues/ViewController.swift
+++ funWithSegues/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		let todoEndpoint: String = "http://jsonplaceholder.typicode.com/todos/1"
+		guard let url = NSURL(string: todoEndpoint) else {
+			print("Error: cannot create URL")
+			return
+		}
+		
+		let urlRequest = NSURLRequest(URL: url)
+		
+		let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+		let session = NSURLSession(configuration: config)
+		
+		let task = session.dataTaskWithRequest(urlRequest) {
+			(data, response, error) in
+			guard error == nil else {
+				print("Error calling GET")
+				print(error)
+				return
+			}
+			
+			guard let responseData = data else {
+				print("Error: did not receive any data")
+				return
+			}
+			do {
+				guard let todo = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as? [String: AnyObject] else {
+					print("Error trying to convert data to JSON")
+					return
+				}
+				print("The todo is: " + todo.description)
+			} catch {
+				print("error")
+			}
+		}
+		
+		task.resume()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 }
 
